6bbd85d3305b9abcdcb4828e46bdc232f0c5701e
Time_thp <thanaphon.thpcd@gmail.com>
2025-09-06 22:56:01 +0700

    setup

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -7,6 +7,7 @@ const Env = z.object({
     .default('development'),
   DATABASE_URL: z.url(),
   PORT: z.coerce.number().default(3000),
+  APP_ORIGIN: z.url().default('http://localhost:3000'),
 });
 
 export const config = Env.parse(process.env);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,31 @@
+import cookieParser from 'cookie-parser';
 import { config } from './libs/config';
 import { prisma } from './libs/prisma';
 import express from 'express';
+import cors from 'cors';
+import errorHandler from './middlewares/errorHandler';
+import { OK } from './libs/http'
+import catchErrors from './utils/catchErrors';
 
+const PORT = config.PORT || 4000;
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: config.APP_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get("/" , (req , res) => {
+  return res.status(OK).json({
+    status:"healthy"
+  });
+})
+app.use(errorHandler);
 
 async function start() {
   try {
