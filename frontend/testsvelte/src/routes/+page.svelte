becba9a9a840e2293b7108c5353d7f85cd81e04d
Bocchi <singha2608@gmail.com>
2025-09-11 02:35:43 +0700

    add Register component (skeleton)

--- frontend/testsvelte/src/routes/+page.svelte
+++ frontend/testsvelte/src/routes/+page.svelte
@@ -5,5 +5,5 @@
     @reference "tailwindcss";
 </style>
 <div class="flex md:flex-1 md:w mr-4 ml-4 rounded-3xl h-30 justify-center"> <img src={favicon} alt="favicon"></div>
-<h1 class="text-6xl m-5 font-bold bg-gradient-to-r from-blue-500 to-green-300 w-xl bg-clip-text text-transparent">test templete</h1>
+<h1 class="text-6xl m-5 font-bold bg-gradient-to-r from-blue-500 to-green-300 w-xl bg-clip-text text-transparent">test template</h1>
 <p class="m-5 text-xl">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
