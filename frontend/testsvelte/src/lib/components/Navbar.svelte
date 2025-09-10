c2d24d695c629a3fb76745b242a6f9e9d204dc7f
RealSoSity <realsosity@gmail.com>
2025-09-10 18:24:54 +0700

    Svelte + tailwindcss test

---
+++ frontend/testsvelte/src/lib/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+</style>
+
+<nav class="font-mono bg-gray-300 bg-auto md:bg-contain mx-4 mt-4 p-4 rounded-md justify-between flex align-middle drop-shadow-lg">
+    <span class="uppercase font-sans bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent text-3xl font-bold">LOGO</span>
+    <ul class="flex space-x-4 md:space-x-8 items-center text-gray-700 text-xl">
+        <li><a href="/" class="hover:underline">Home</a></li>
+        <li><a href="/" class="hover:underline">About</a></li>
+    </ul>
+</nav>
--- frontend/testsvelte/src/routes/+page.svelte
+++ frontend/testsvelte/src/routes/+page.svelte
@@ -1,10 +1,6 @@
-<h1>test templete</h1>
+<h1 class="text-6xl m-5 font-bold bg-gradient-to-r from-blue-500 to-green-300 w-xl bg-clip-text text-transparent">test templete</h1>
 
-<style>
-    h1 {
-        color: red;
-        font-size: 50px;
-        font-style:  italic;
-    }
+<style lang="postcss">
+    @reference "tailwindcss";
 </style>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p class="m-5 text-xl">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
