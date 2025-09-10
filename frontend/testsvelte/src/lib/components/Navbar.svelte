af84d5f3e513fe1a87a6675f89e181d73e803c4a
RealSoSity <realsosity@gmail.com>
2025-09-10 18:49:29 +0700

    Nav update

--- frontend/testsvelte/src/lib/components/Navbar.svelte
+++ frontend/testsvelte/src/lib/components/Navbar.svelte
@@ -9,7 +9,7 @@
 <nav class="font-mono bg-gray-300 bg-auto md:bg-contain mx-4 mt-4 p-4 rounded-md justify-between flex align-middle drop-shadow-lg">
     <span class="uppercase font-sans bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent text-3xl font-bold">LOGO</span>
     <ul class="flex space-x-4 md:space-x-8 items-center text-gray-700 text-xl">
-        <li><a href="/" class="hover:underline">Home</a></li>
-        <li><a href="/" class="hover:underline">About</a></li>
+        <li><a href="/" class="hover:underline">Dashboard</a></li>
+        <li><a href="/" class="hover:underline">Transaction</a></li>
     </ul>
 </nav>
