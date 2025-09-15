842d6e7c735cd057cc2fa07a64102ff57f5c5af9
Bocchi <singha2608@gmail.com>
2025-09-16 05:22:40 +0700

    in home compare line expense,income to analytic

---
+++ frontend/testsvelte/src/routes/analytics/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+    
+</script>
+<h1 style="color: black;" class="text-5xl flex justify-center flex-col font-bold text-center mt-20 text-gray-900 dark:text-white">Welcome to analytics,</h1>
--- frontend/testsvelte/src/routes/home/+page.svelte
+++ frontend/testsvelte/src/routes/home/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-    import logo from "$lib/assets/expenTrack_logo.svg";
+	import Card from "$lib/components/Compare-line.svelte";
 
-    let mode = $state('');
 </script>
 <h1 style="color: black;" class="text-5xl flex justify-center flex-col font-bold text-center mt-20 text-gray-900 dark:text-white">Welcome to home,</h1>
+
+<div class=" mx-4 mt-4 p-10 rounded-md justify-between flex align-middle drop-shadow-lg">
+<Card></Card>
+</div>
